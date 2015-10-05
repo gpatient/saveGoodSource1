ed5a8dffade3b3f96493c705a4c50a278ca61fb7
gpatient <gpatient@naver.com>
2015-10-06 03:38:19 +0800

    study_CanChangDrumSpeed

--- index.js
+++ index.js
@@ -13,7 +13,7 @@
 
  */
 import dbg from 'debug';
-dbg('aaa')(3.3^15.3);
+dbg('aaa')((32.44>>1)>>1);
 
 function LLT(t) {
 
@@ -32,9 +32,12 @@ function LLT(t) {
                  //=t/2 %4 2 second for background one array.........  (8192/8000 bpm )
                 y = Math.pow(2, backgroundWaveNotes[t >> 14 & 3] / 12);
 
+                //0x800 = 256*8= 2^11 .......
+                var drumSpeed=0x12ff;
                 //for drum
                 // a= 1/4 beat saw evelope
-                a = 1 - ((t & 0x7ff) /  0x7ff);
+                //a = 1 - ((t & 0x7ff) /  0x7ff);
+                a = 1 -(t%drumSpeed)/drumSpeed;
                 // d= high freq noise...value  0~2047 for hihat
                 d = (( 14 * t * t ^ t) & 0x7ff);
                 //0x800=256*8=2048=2^11 t*8000%2048/2047 = t%0.25   1/4 beat decay envlove... 
@@ -50,16 +53,16 @@ function LLT(t) {
                         + (Math.floor(y * t * 0.25) & 127 - 64) * 1.2;
                 var drum = (
                                // base drum 8000*5*t %2048  (1.25,4) beat...  saw wave drum 
-                                (Math.floor((Math.floor(5 * t) & 0x7ff) *a) & 255 - 127)
+                                (Math.floor((Math.floor(5 * t) % drumSpeed) *a) & 255 - 127)
                                 //32bit drum score 1001 0011 0010 0011 0010 0011 0010 0011   4 hz speed
                                 //  bit reverse    1100 0100 1100 0100 1100 0100 1100 1001
-                                * ((0x53232323 >> (t >> 11 & 31)) & 1) * a * 1.0
+                                * ((0x53232323 >> ( ((t /drumSpeed)) & 31)) & 1) * a * 1.0
                                 //hihat 
                                 + (Math.floor(d * a) & 255 - 128)
                                //   0xa444c444 is  1010 1000 1000 1000 1100 1000 1000 1000
                                //     bit reverse  0001 0001 0001 0011 0001 0001 0001 0101
-                                * ((0xa444c444 >> (t >> 11 & 31)) & 1) * a * 1.5 + (Math.floor((a
-                                                        * a * d * (t >> 9 & 1))) & 0xff - 0x80) * 0.1337)
+                                * ((0xa444c444 >> (t /drumSpeed & 31)) & 1) * a * 1.5 + (Math.floor((a
+                                                        * a * d * (t /(drumSpeed/4) & 1))) & 0xff - 0x80) * 0.1337)
                         ;//* sb;
 
                 // 8000/2pi= 1300 hz ..... 2600hz  650 hz  (1300,2600,652,1301) hz saw wave vol 31*4  g is evelope
@@ -74,7 +77,7 @@ function LLT(t) {
         var out;
         //cut value (clip) ~128 ~ 127......
         out=Math.max(Math.min((wave + drum + instrument) / 3, 127), -128);
-       // out=Math.max(Math.min((drum) / 3, 127), -128);
+        //out=Math.max(Math.min((drum) / 3, 127), -128);
         //out =d/2048*128;
     return out;
 }
